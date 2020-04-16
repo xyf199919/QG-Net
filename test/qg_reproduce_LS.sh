79e86850c858c9d39f8ed970a9910c88d2ef43b6
xyf199919 <40393702+xyf199919@users.noreply.github.com>
2020-04-16 02:08:44 -0700

    script

--- test/qg_reproduce_LS.sh
+++ test/qg_reproduce_LS.sh
@@ -8,13 +8,13 @@ debug='False'
 selection_criterion='NLL'
 model_dir="models.for.test"
 #model="model_acc_53.29_ppl_13.59_e20.pt"
-model="LSTM-attn.pt"
+model="QG-Net.pt"
 alpha=0.2
 beta=0.2
 
 python3 ../OpenNMT-py/generate.py \
 -model $model_dir/$model \
--src input.for.test/input.txt \
+-src input.for.test/inputformat.txt \
 -output output_questions_$model.txt \
 -dynamic_dict \
 -verbose -batch_size 1 -gpu 0 -beam_size $beam_size -replace_unk -n_best $nbest \
